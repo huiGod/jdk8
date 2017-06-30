03cbc112ca31b4aef5e76ae3dd6df13fd5a48be6
“huiGod <tenghuiit@163.com>
2017-06-30 22:40:39 +0800

    java8

---
+++ src/main/java/com/huigod/java8/stream/MyCustomCollector2.java
@@ -0,0 +1,61 @@
+package com.huigod.java8.stream;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class MyCustomCollector2<T> implements Collector<T, Set<T>, Map<T, T>> {
+    @Override
+    public Supplier<Set<T>> supplier() {
+        System.out.println("supplier invoked!");
+        return HashSet::new;
+    }
+
+    @Override
+    public BiConsumer<Set<T>, T> accumulator() {
+        System.out.println("accumulator invoked!");
+        return (set1, set2) -> {
+            System.out.println("accumylator: " + set1 + "," + Thread.currentThread().getName());
+            set1.add(set2);
+        };
+    }
+
+    @Override
+    public BinaryOperator<Set<T>> combiner() {
+        System.out.println("combiner invoked!");
+        return (set1, set2) -> {
+            set1.addAll(set2);
+            return set1;
+        };
+    }
+
+    @Override
+    public Function<Set<T>, Map<T, T>> finisher() {
+        System.out.println("finisher invoked!");
+        return set -> {
+            Map<T, T> map = new HashMap<>();
+            set.stream().forEach(item -> map.put(item, item));
+            return map;
+        };
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        System.out.println("characteristics invoked!");
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.UNORDERED, Characteristics.CONCURRENT));
+    }
+
+    public static void main(String[] args) {
+        for (int i =0; i<1; i++) {
+            List<String> list = Arrays.asList("hello", "world", "welcome", "hello", "a", "b", "d", "e", "f");
+            Set<String> set = new HashSet<>();
+            set.addAll(list);
+
+            Map<String, String> map = set.parallelStream().collect(new MyCustomCollector2<>());
+            System.out.println(map);
+        }
+    }
+}
